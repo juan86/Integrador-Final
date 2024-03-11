1089a9631ac5fb28cc4434113f59ee2838391b3e
Juan Mercado <juan.mercado1986@gmail.com>
2024-03-11 13:16:49 -0300

    Primer commit

---
+++ src/test/java/com/juanmercado/integradorfinal/IntegradorFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanmercado.integradorfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
